c8f96ea37dc45e807ba1c250c71d5829b0862655
thbst16 <thomas@beckshome.com>
2021-03-08 22:29:50 -0500

    Updated function to pull beers

--- BeerTrigger.cs
+++ BeerTrigger.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -13,22 +15,40 @@ namespace com.beckshome.function
     {
         [FunctionName("BeerTrigger")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var breweryDbApiKey = Environment.GetEnvironmentVariable("BREWERY_DB_API_KEY");
+            var client = new HttpClient();
+            var response = await client.GetAsync($"https://sandbox-api.brewerydb.com/v2/beer/random?key={breweryDbApiKey}");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseRoot = JsonConvert.DeserializeObject<Root>(responseString);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return (ActionResult)new OkObjectResult(responseRoot.Data);
         }
+    }
+
+    public class Root
+    {
+        public string Message { get; set; }
+        public Beer Data { get; set; }
+        public bool Success { get; set; }
+    }
+
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string ABV { get; set; }
+        public Style Style { get; set; }
+    }
+
+    public class Style
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
